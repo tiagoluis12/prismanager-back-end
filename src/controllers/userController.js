50b60f4f56e1791a611b1e905cc0e174164f6d67
Patricia Eihara <pateihara@gmail.com>
2023-10-16 21:15:36 -0300

    bcrypt2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 import UserSchema from "../models/userSchema.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const SECRET = process.env.SECRET;
